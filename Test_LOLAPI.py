6a6d3c5b43c8cfc8d53f3c02f0b3c111ef90fabb
synchrowisdom <40252080+synchrowisdom@users.noreply.github.com>
2018-07-22 23:03:06 -0400

    Create Test_LOLAPI.py
    A simple program to show how to use the LOLAPI class.

---
+++ Test_LOLAPI.py
@@ -0,0 +1,81 @@
+'''
+
+
+Examples using the LOLAPI Python wrapper.
+For more info about the API service, consult 
+https://developer.riotgames.com/getting-started.html.
+
+NB: It only supports the 'NA' region at this time.
+
+
+'''
+
+
+from LOLAPI import LOLAPI
+import json
+
+# Display some documentation about each functions.
+print('LOLAPI(apikey=, region=\'NA\')')
+print(LOLAPI.__init__.__doc__)
+print('')
+print('get_summonerbyname(summonername=)')
+print(LOLAPI.get_summonerbyname.__doc__)
+print('')
+print('get_currentmatch(summonerid=)')
+print(LOLAPI.get_currentmatch.__doc__)
+print('')
+print('get_recentmatches(accountid=)')
+print(LOLAPI.get_recentmatches.__doc__)
+
+
+
+# Configure your API key here and initialize the class.
+api = LOLAPI('Your developper API key')
+summonerName = 'ASummonerName'
+
+# Get the summoner information.
+status_code, text = api.get_summonerbyname(summonerName)
+if status_code == 200:    
+    summoner = json.loads(text)
+    print('get_summonerbyname:')
+    print(json.dumps(summoner, indent=4))
+    print('')
+else:
+    # Seomthing went wrong, is this summoner name valid?
+    print('Couldn\'t retrieve this summoner\'s information. Please check the summoner name and retry (NA region only).')
+    exit(-1)
+
+
+# Get the current match information for 
+#   a particular summonerId. PS: Summoner must be
+#   in-game.
+status_code, text = api.get_currentmatch(summoner['id'])
+if status_code == 200:   
+    currentmatch = json.loads(text)
+    participants = currentmatch['participants']
+    print('get_currentmatch:')
+    print(json.dumps(participants, indent=4))
+    print('')
+    
+    # Pull the list of summoner as well as the summonerId they are currently playing.
+    print('get_currentmatch summonerName and summonerId:')
+    for p in participants:
+        print(p['summonerName'], p['summonerId'])
+    print('')
+else:
+    # Something went wrong, is this summoner in a game right now?
+    print('Couldn\'t retrieve the current game for this summoner.')
+
+
+# Get the recent matches list for
+#   a particular accountid.
+status_code, text = api.get_recentmatches(summoner['accountId'])
+if status_code == 200:
+    recentmatches = json.loads(text)
+    print('get_recentmatches:')
+    print(json.dumps(recentmatches, indent=4))
+    print('')
+else:
+    # Something went wrong, is this summoner valid or perhaps never played yet.
+    print('Couldn\'t retrive recent matches for this summoner.')
+
